5c16e2e881acaacc3523d9a8795695db8ef9e8f9
Federico Nicolas Pisciolari Chauvie <federicopisciolari@gmail.com>
2026-02-15 13:48:47 -0300

    Styled-Components-15022026

---
+++ styledcomponents/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+//Importamos el Compoente:
+import { MiCaja } from './syled';
+
+// Itilizamos el Compoente Importando anteriormente:
+function App() {
+  return (
+    <div className="App">
+
+      <MiCaja>Entrar</MiCaja>
+
+    </div>
+  );
+}
+
+export default App;
+
+
+
